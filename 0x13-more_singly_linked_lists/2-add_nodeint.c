db5e6fcccbbee0eb0dde03fd8bdae103fc362bfc
SamKinyua <samkinyua.w@gmail.com>
2023-04-04 16:10:01 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,20 +4,20 @@
  * add_nodeint - Adds a node at the begining of a listint_t
  * @n: data type pointer of struct
  * @head: data type pointer of the head/next node
- * Return: elements of str node_mpya
+ * Return: elements of str new_node
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *node_mpya;
+	listint_t *new_node;
 
-	node_mpya = malloc(sizeof(listint_t));
-	if (node_mpya == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	node_mpya->n = n;
-	node_mpya->next = *head;
-	*head = node_mpya;
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (new_node);
 }
