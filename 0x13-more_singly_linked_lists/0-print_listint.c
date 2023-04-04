fcb86555d9519d5a6c505ee26fe54ae4e296e88d
SamKinyua <samkinyua.w@gmail.com>
2023-04-04 13:25:29 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print elements of list_int list
+ * @h: struct data type pointer
+ * Return: str i elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i = 0;
+	const listint_t *new_node = h;
+
+	for (; new_node; new_node = new_node->next)
+	{
+		printf("%d\n, new_node->n);
+		i++;
+	}
+	return(i);
+}
