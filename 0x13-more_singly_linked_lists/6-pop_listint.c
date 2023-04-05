5bad85d0aa72c8a025450ce833c54010d424faa1
SamKinyua <samkinyua.w@gmail.com>
2023-04-04 17:38:45 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * pop_listint - pop the first value
- * @head: the list
- * Return: int value
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: pointer to first node
+ *
+ * Return: data in @head
  */
 
 int pop_listint(listint_t **head)
@@ -12,13 +13,12 @@ int pop_listint(listint_t **head)
 	int n;
 
 		if (!*head || !head)
-			return (0);
+		return (0);
 
-		new = *head;
-		*head = (*head)->next;
-		n = new->n;
-		free(new);
+	new = *head;
+	*head = (*head)->next;
+	n = new->n;
+	free(new);
 
-		return (n);
+	return (n);
 }
-
