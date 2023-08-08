ada3eacb139b43c332a0ecde2c2262729cc860fd
SamKinyua <samkinyua.w@gmail.com>
2023-08-07 23:46:20 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads textfile and prints to standard output
+ * @filename: name of file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters printed or 0 if unsuccessful
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t x, y, z;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	x = open(filename, O_RDONLY);
+	y = read(x, buffer, letters);
+	z = write(STDOUT_FILENO, buffer, y);
+
+	if (x == -1 || y == -1 || z == -1 || z != y)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(x);
+
+	return (z);
+}
