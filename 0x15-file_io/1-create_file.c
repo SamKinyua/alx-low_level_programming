42fc43bbb50f82639cf2cdbdd0b63060253a3250
SamKinyua <samkinyua.w@gmail.com>
2023-08-08 01:50:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,27 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int i = 0;
-	int retval = 1;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename,  0_WRONLY | 0_CREAT | 0_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	while (text_content[i])
-		i++;
-	retval = write(fd, text_content, != i)
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
+
+	close(o);
+
 	return (1);
 }
-
