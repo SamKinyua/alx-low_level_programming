21511541163add5e218bc982651d3a1f2cdcc0a6
SamKinyua <samkinyua.w@gmail.com>
2023-09-28 14:35:02 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_bit - returns bit value at gven index
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ *
+ * Return: value of the bit, -1 in case of error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int r;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (r = 0; r <= 63; n >>= 1, r++)
+	{
+		if (index == r)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
