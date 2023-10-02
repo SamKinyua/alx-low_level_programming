b647d7062c474db6a2d5e3a902e16170dbb258e5
SamKinyua <samkinyua.w@gmail.com>
2023-10-02 16:14:52 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,34 @@
 #include "main.h"
 
-
 /**
-* create_file - creates file with text
-* @filename: name of file
-* @text_content: text in the file
-*
-* Return: 1 if successful otherwise -1
-*/
+ * create_file - creates file with text
+ * @filename: name of file
+ * @text_content: text in the file
+ *
+ * Return: 1 if successful otherwise -1
+ */
+
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int cntnt;
-	int rer;
+		int fd;
+		int i = 0;
+		int retval = 1;
 
-	if (!filename)
-		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
-		text_contnet = "";
-
-	for (cntnt = 0; text_content[cntnt]; cntnt++)
-		;
-
-	rer = write(fd, text_content, cntnt);
-
-	if (rer == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+		if (!filename)
+			return (-1);
+		fd = open(filenmame, 0_WRONLY | 0_CREAT | 0_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
+		if (!text_content)
+		{
+			close(fd);
+			return (1);
+		}
+		while (text_content[i])
+			i++;
+		retval = write(fd, text_content, != i)
+			return (-1);
+		return (1);
 }
