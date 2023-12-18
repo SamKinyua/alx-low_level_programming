1db95027aa22a356879cda489296cf4534552277
SamKinyua <samkinyua.w@gmail.com>
2023-12-17 21:11:52 -0800

    Betty Fixes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-	
+
 
 /**
  * read_textfile - Reads textfile and prints to standard output.
@@ -9,36 +9,36 @@
  *
  * Return: number of letters printed or 0 if unsuccessful.
  */
-	
+
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t x, y, z;
 	char *buffer;
-	
+
 
 	if (filename == NULL)
 		return (0);
-	
+
 
 	buffer = malloc(sizeof(char) * letters);
-	
+
 
 	if (buffer == NULL)
 		return (0);
-	
+
 
 	x = open(filename, O_RDONLY);
 	y = read(x, buffer, letters);
 	z = write(STDOUT_FILENO, buffer, y);
-	
+
 
 	if (x == -1 || y == -1 || z == -1 || z != y)
 	{
 		free(buffer);
 		return (0);
 	}
-	
+
 
 	free(buffer);
 	close(x);
