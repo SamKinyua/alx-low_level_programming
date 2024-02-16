a5216be79dbf723f96659ce157d2580b4b3f2f94
SamKinyua <samkinyua.w@gmail.com>
2024-02-16 14:00:15 -0800

    betty corrected

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * key_index - function that generates distributed index according
- * to given hash
+ * key_index - funtions that generates a distributed index according
+ * to a given hash
  *
  * @key: key passed
  * @size: size of hash tables
@@ -11,6 +11,6 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	if (key == NULL || size == 0)
-	       return (0);
+		return (0);
 	return (hash_djb2(key) % size);
 }
