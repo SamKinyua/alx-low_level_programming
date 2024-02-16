cc5cd8028f4dd5ff8903e2b23e23cf5811b627c2
SamKinyua <samkinyua.w@gmail.com>
2024-02-16 13:45:41 -0800

    compiled files

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,6 +18,7 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		tmp1 = ht->array[i];
+		while ((tmp2 = tmp1) != NULL)
 		{
 			tmp1 = tmp1->next;
 			free(tmp2->key);
